54b5c27970de177a189591981902a91f6c5e0749
prakhar8827 <51355934+prakhar8827@users.noreply.github.com>
2019-06-10 09:48:47 +0530

    Create calci.java

---
+++ calci.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class calci{
+public static void main(String []args){
+System.out.println("Enter two numbers to find their sum:");
+Scanner n1 = new Scanner(System.in);
+int a = n1.nextInt();
+int b = n1.nextInt();
+System.out.println("Sum is:"+(a+b));
+}
+}
