b72652028411a431a36f97d7e11d3aa83721c9fd
prakhar8827 <51355934+prakhar8827@users.noreply.github.com>
2019-06-10 11:47:49 +0530

    Update calci.java

--- calci.java
+++ calci.java
@@ -3,8 +3,8 @@ class calci{
 public static void main(String []args){
 System.out.println("Enter two numbers to find their sum:");
 Scanner n1 = new Scanner(System.in);
-int a = n1.nextInt();
-int b = n1.nextInt();
+int a = n1.nextInt(1);
+int b = n1.nextInt(2);
 System.out.println("Sum is:"+(a+b));
 }
 }
