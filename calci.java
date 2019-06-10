d510dbc51e2d5aac1975f90daf69a3a1d720a2c7
prakhar8827 <51355934+prakhar8827@users.noreply.github.com>
2019-06-10 11:52:30 +0530

    Update calci.java

--- calci.java
+++ calci.java
@@ -2,9 +2,17 @@ import java.util.*;
 class calci{
 public static void main(String []args){
 System.out.println("Enter two numbers to find their sum:");
-Scanner n1 = new Scanner(System.in);
-int a = n1.nextInt(1);
-int b = n1.nextInt(2);
-System.out.println("Sum is:"+(a+b));
+try{
+int a = parseint(args[0]);
+int b = parseint(args[1]);
+  System.out.println("Sum is:"+(a+b));
+}catch(Exception e)
+{
+  System.out.println("error");
+}
+  finally{
+    System.out.println("wew");
+  }
+
 }
 }
